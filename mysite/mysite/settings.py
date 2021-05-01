432bd8ba38bdf9df48d37b48042d0c5b746bf317
sagar-code <sagarfmt@gmail.com>
2021-05-01 21:29:58 +0545

    upto generic view completed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,6 +15,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -89,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
